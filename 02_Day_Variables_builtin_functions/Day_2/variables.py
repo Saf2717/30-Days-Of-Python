935df4e4b5ef6c77846490ce8ee6d63ca27aee52
safwanul sikder <ss2766@kent.ac.uk>
2026-08-19 15:01:59 +0100

    Create variables.py
    Day1

---
+++ 02_Day_Variables_builtin_functions/Day_2/variables.py
@@ -0,0 +1,27 @@
+#Day 2: 30 Days of python programming
+
+first_name = 'Saf'
+second_name = 'Sikder'
+full_name = first_name + ' ' + second_name
+country = 'Bangladesh'
+city = 'Dhaka'
+age = 20
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+colour, height, weight = 'red', 5.9, 60
+
+
+print(type(first_name))
+print(len(first_name))
+print(len(second_name), len(first_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+modulus = num_one % num_two
+variable_exp = num_one ** num_two
+
